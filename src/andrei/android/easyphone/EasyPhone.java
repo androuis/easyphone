eed60d8a45ebd5b2e86a2e833be8724d1841ea17
Andrei Bacalu <bacaluandrei@gmail.com>
2012-09-15 20:59:28 +0300

    the first commit

---
+++ src/andrei/android/easyphone/EasyPhone.java
@@ -0,0 +1,15 @@
+package andrei.android.easyphone;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class EasyPhone extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
